947139997361e81b2d9d6492bd523a92ce6f66b6
Mathieu Charles <119063962+CharlesMathieu@users.noreply.github.com>
2023-01-27 09:55:31 +0100

    Create Terre11.js

---
+++ Terre11.js
@@ -0,0 +1,17 @@
+//Déterminer si un nombre est premier
+function isPrime(n) {
+    if (n <= 1) return false
+  
+    for (let i = 2; i < n; i++) {
+      if (n % i === 0) return false
+    }
+  
+    return true
+  }
+  
+
+  console.log(isPrime(1)) 
+  console.log(isPrime(3)) 
+  console.log(isPrime(4)) 
+  console.log(isPrime(5)) 
+  
