c03f459ce6cb5ed7ffe84136bf927276ea9b2688
Mathieu Charles <119063962+CharlesMathieu@users.noreply.github.com>
2023-01-27 09:47:49 +0100

    Update Terre01.js

--- Terre01.js
+++ Terre01.js
@@ -1,5 +1,9 @@
-let alphabet ="abcdefghijklmnopqrstuvwxyz"
-
-for(let i = 0; i<1; i++){
-    console.log(alphabet)
+const alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+let AZ = ''
+//Je boucle mon tableau
+//A chaque tour de boucle j'ajoute une lettre
+//à ma variable AZ
+for(let i = 0; i<alphabet.length; i++){
+AZ += alphabet[i]
 }
+console.log(AZ)
